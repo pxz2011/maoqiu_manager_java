1e9eb46faa7dd2c560749d48afcae3c3b69d59c2
pxz2011 <1521955177@qq.com>
2023-12-21 18:58:42 +0800

    init

---
+++ src/main/java/org/pxzq/maoqiumanager/entity/UserInfoEntity.java
@@ -0,0 +1,12 @@
+package org.pxzq.maoqiumanager.entity;
+
+/**
+ * @author pxz
+ * @version 1.0
+ * @project maoqiu-manager
+ * @description 用户信息表Entity
+ * @date 2023/12/21 18:45:29
+ */
+public class UserInfoEntity {
+
+}
