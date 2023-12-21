2a4a0d9b476879c7e432ad5247b8aebb01d2381a
pxz2011 <1521955177@qq.com>
2023-12-21 18:48:54 +0800

    init

--- src/main/java/org/pxzq/maoqiumanager/MaoqiuManagerApplication.java
+++ src/main/java/org/pxzq/maoqiumanager/MaoqiuManagerApplication.java
@@ -1,8 +1,12 @@
 package org.pxzq.maoqiumanager;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author ASUS
+ */
 @SpringBootApplication
 public class MaoqiuManagerApplication {
 
