bae3492c4ef2bd7ccd61db6cf878119e26b4cff8
pxz2011 <1521955177@qq.com>
2023-12-21 18:48:02 +0800

    init

---
+++ src/main/java/org/pxzq/maoqiumanager/MaoqiuManagerApplication.java
@@ -0,0 +1,14 @@
+package org.pxzq.maoqiumanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaoqiuManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaoqiuManagerApplication.class, args);
+    }
+
+}
+    
---
+++ src/test/java/org/pxzq/maoqiumanager/MaoqiuManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pxzq.maoqiumanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaoqiuManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
